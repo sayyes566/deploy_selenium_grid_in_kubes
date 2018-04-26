8981e7bdbae979a5a70186e3294f57c9c452e0b3
coding dog <sayyes566@gmail.com>
2018-04-26 17:24:45 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,14 +5,14 @@ import sys, argparse
 
 class SearchText(unittest.TestCase):
    def setUp(self):
-        self.test_ip=args.input
-        print( self.test_ip)
+        self.test_url=args.testurl
+        print( 'test url = '+ self.test_url)
+        print( 'webdriver url = http://'+ args.driverip+ ':4444/wd/hub')
         # create a new session
         self.chrome = webdriver.Remote(
-            command_executor='http://35.229.159.251:4444/wd/hub',
-            #desired_capabilities=DesiredCapabilities.FIREFOX)
+            command_executor='http://' + args.driverip + ':4444/wd/hub',
             desired_capabilities=DesiredCapabilities.CHROME)
-        self.chrome.get('http://' + self.test_ip)
+        self.chrome.get(self.test_url )
    def test_search_by_text(self):
         #lists = self.chrome.find_elements_by_class_name("w10")
         #no=len(lists)
@@ -23,10 +23,10 @@ class SearchText(unittest.TestCase):
         self.chrome.quit()
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', default='My Input')
+    parser.add_argument('--driverip', default='My web driver ip')
+    parser.add_argument('--testurl', default='My test url')
     parser.add_argument('unittest_args', nargs='*')
     args = parser.parse_args()
     sys.argv[1:] = args.unittest_args
     unittest.main()
-
 
